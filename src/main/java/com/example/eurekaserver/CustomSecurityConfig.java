79b78aaffe80afe022f7bc30a4975988751a5a72
“monkey-kenny” <56151899@qq.com>
2019-12-22 19:15:39 +0800

    服务中心集群配置

--- src/main/java/com/example/eurekaserver/CustomSecurityConfig.java
+++ src/main/java/com/example/eurekaserver/CustomSecurityConfig.java
@@ -1,4 +1,27 @@
 package com.example.eurekaserver;
 
-public class CustomSecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 自定义
+ */
+@Configuration
+@EnableWebSecurity
+public class CustomSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //关闭csrf
+        http.csrf().disable();
+
+        http.authorizeRequests().
+                anyRequest().
+                authenticated(). //表示任何请求都要经过认证后才能访问
+                and().
+                formLogin(). //开启表单认证方式
+                and().
+                httpBasic(); //支持Http Basic认证
+    }
 }
--- src/main/java/com/example/eurekaserver/EurekaEventListener.java
+++ src/main/java/com/example/eurekaserver/EurekaEventListener.java
@@ -1,4 +1,53 @@
 package com.example.eurekaserver;
 
+import org.springframework.cloud.netflix.eureka.server.event.*;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class EurekaEventListener {
+
+    /**
+     * 服务下线事件
+     * @param event
+     */
+    @EventListener
+    public void listenCancelEvent(EurekaInstanceCanceledEvent event){
+        System.out.println(event.getAppName()+"下线啦~");
+    }
+
+    /**
+     * 服务注册事件
+     * @param event
+     */
+    @EventListener
+    public void listenRegisterEvent(EurekaInstanceRegisteredEvent event){
+         System.out.println(event.getInstanceInfo().getAppName()+"服务正在注册");
+    }
+
+    /**
+     * 服务续约事件
+     * @param event
+     */
+    @EventListener
+    public void listenRenewedEvent(EurekaInstanceRenewedEvent event){
+        System.out.println(event.getAppName()+"服务续约成功");
+    }
+
+    /**
+     * 服务中心启动事件
+     * @param event
+     */
+    @EventListener
+    public void listenRegistryAvailableEvent(EurekaRegistryAvailableEvent event){
+        System.out.println("服务中心启动~");
+    }
+    /**
+     * Eureka Server启动事件
+     * @param event
+     */
+    @EventListener
+    public void listenServerStartedEvent(EurekaServerStartedEvent event){
+        System.out.println("Eureka Server 启动~");
+    }
 }
